f850ef38dc6d0c4b82c04581fec95d32e9185d62
Raul Bermudez <raulbego16@gmail.com>
2025-05-05 20:26:01 +0200

    Cambio el contenido de la migracióm

--- database/migrations/2025_05_05_182421_create_ciclos_disponibles_table.php
+++ database/migrations/2025_05_05_182421_create_ciclos_disponibles_table.php
@@ -9,13 +9,29 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('ciclos_disponibles', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->string('nombre', 10)->primary();
         });
+
+        // Insertar los datos
+        DB::table('ciclos_disponibles')->insert([
+            ['nombre' => '1 ASIR A'],
+            ['nombre' => '1 ASIR B'],
+            ['nombre' => '1 DAMP A'],
+            ['nombre' => '1 DAMP B'],
+            ['nombre' => '1 DAW A'],
+            ['nombre' => '1 DAW B'],
+            ['nombre' => '2 ASIR A'],
+            ['nombre' => '2 ASIR B'],
+            ['nombre' => '2 DAMP A'],
+            ['nombre' => '2 DAMP B'],
+            ['nombre' => '2 DAW A'],
+            ['nombre' => '2 DAW B'],
+        ]);
     }
+
 
     /**
      * Reverse the migrations.
